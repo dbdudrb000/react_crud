526faddc5abe92f36056cde7c8fb95da65651b1c
YYG <dbdudrb00@naver.com>
2024-01-16 20:23:14 +0900

    css Update

--- src/header/top_box/TopBox.jsx
+++ src/header/top_box/TopBox.jsx
@@ -16,30 +16,32 @@ const TopBox = () => {
 
     return (
         <React.Fragment>
-            <BrowserRouter>
-                <Routes>
-                    <Route path={'login'} element={<SignInCompoment />} />
-                    <Route path={'logup'} element={<SignUpCompoment />} />
-                </Routes>
-            
-            <div className="top-containner-box">
-                <div className="top-title">
-                    <h2>고민상담</h2>
+            <header>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path={'login'} element={<SignInCompoment />} />
+                        <Route path={'logup'} element={<SignUpCompoment />} />
+                    </Routes>
+                
+                <div className="top-containner-box">
+                    <div className="top-title">
+                        <h2>고민상담</h2>
+                    </div>
+                    <div className="top-login-box">
+                        <nav>
+                            <ul>
+                                <li>
+                                    <Link to="login">로그인2</Link>
+                                </li>
+                                <li>
+                                    <a onClick={() => { alert('회원가입 버튼입니다.'); }}>회원가입</a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
                 </div>
-                <div className="top-login-box">
-                    <nav>
-                        <ul>
-                            <li>
-                                <Link to="login">로그인2</Link>
-                            </li>
-                            <li>
-                                <a onClick={() => { alert('회원가입 버튼입니다.'); }}>회원가입</a>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-            </div>
-            </BrowserRouter>
+                </BrowserRouter>
+            </header>
         </React.Fragment>
     );
 }
