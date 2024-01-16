06d0bab6f1fb8e8e4c01c3088f278df9368c6a92
YYG <dbdudrb00@naver.com>
2024-01-16 17:14:12 +0900

    top_box developing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import TopBox from './header/top_box/TopBox';
+import WorryList from './header/worry/worry_list/WorryList';
 
 function App() {
   return (
     <React.Fragment>
+      <div className='wrap'>
+        <div className='inner'>
+          <header>
+            <div className='inner-top-box'>
+              <TopBox />
+            </div>
+            <div className='worry-kind'>
+              <WorryList />
+            </div>
+          </header>
+          <div className='contents'>
 
+          </div>
+          <footer>
 
+          </footer>
+
+        </div>
+      </div>
     </React.Fragment>
   );
 }
