c8e129c75809cdfcf7b6e7786ff0f47371985c24
YYG <dbdudrb00@naver.com>
2024-01-17 21:21:56 +0900

    worry Ui Update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ import SearchBox from '../layout/SearchBox';
 import WorryBox from '../layout/WorryBox';
 
 const HomePage = () => {
+    console.log('Home Component 들어왓슴');
     return (
         <main>
             <SearchBox attr="search-box" />
